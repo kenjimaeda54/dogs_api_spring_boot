ab94f4ce4d9c218248455e31f7e00e942b978088
kenjimaeda54 <kenjimaedafamily@gmail.com>
2025-01-11 19:00:55 -0300

    feat: started project

---
+++ src/main/kotlin/com/dogs/dogs/api/Application.kt
@@ -0,0 +1,11 @@
+package com.dogs.dogs.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
