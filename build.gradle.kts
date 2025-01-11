c3ec1c339b9f093cdb386abc2fb0994a71bebb61
kenjimaeda54 <kenjimaedafamily@gmail.com>
2025-01-11 20:47:28 -0300

    feat: conected with mysql and configuration databse

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.7"
 	id("io.spring.dependency-management") version "1.1.7"
+
+	//jpa
+	kotlin("plugin.jpa") version "2.1.0"
+
 }
 
 group = "com.dogs"
@@ -19,12 +23,25 @@ repositories {
 }
 
 dependencies {
+	//vem com o intializer
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+	//jpa
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+//	//flyway
+//	implementation("org.flywaydb:flyway-mysql:8.5.13")
+//	implementation("org.flywaydb:flyway-core:11.1.1")
+
+
+
 }
 
 kotlin {
---
+++ src/main/kotlin/com/dogs/dogs/api/presentation/controllers/dogs/DogsController.kt
@@ -0,0 +1,8 @@
+package com.dogs.dogs.api.presentation.dogs
+
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class DogsController {
+}
